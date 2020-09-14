f6f72de6d52ba4eb8f32d3650a7bff0399aafba7
Timo C. Wunderlich <timo.wunderlich@charite.de>
2020-09-14 08:03:19 +0200

    Format commit

--- paper/figure1.py
+++ paper/figure1.py
@@ -5,18 +5,24 @@ from matplotlib.ticker import FormatStrFormatter
 from eventprop.lif_layer import LIFLayer, LIFLayerParameters
 from eventprop.layer import Spike
 
+
 def get_poisson_times(isi, t_max):
     times = [np.random.exponential(isi)]
     while times[-1] < t_max:
-        times.append(times[-1]+np.random.exponential(isi))
+        times.append(times[-1] + np.random.exponential(isi))
     return times[:-1]
+
 
 np.random.seed(6)
 
 hidden_parameters = LIFLayerParameters(n_in=100, n=1)
 output_parameters = LIFLayerParameters(n_in=1, n=1)
-w_hidden = np.random.normal(0.005, 0.05, size=(hidden_parameters.n_in, hidden_parameters.n))
-w_output = np.random.normal(5.0, 0.0, size=(output_parameters.n_in, output_parameters.n))
+w_hidden = np.random.normal(
+    0.005, 0.05, size=(hidden_parameters.n_in, hidden_parameters.n)
+)
+w_output = np.random.normal(
+    5.0, 0.0, size=(output_parameters.n_in, output_parameters.n)
+)
 
 w_eps = 1e-6
 isi = 5e-3
@@ -31,7 +37,7 @@ input_spikes.sort(key=lambda x: x.time)
 
 # Calculate numerical gradient for hidden synapse
 w_plus = w_hidden.copy()
-w_plus[0,0] += w_eps
+w_plus[0, 0] += w_eps
 hidden_layer = LIFLayer(hidden_parameters, w_plus)
 output_layer = LIFLayer(output_parameters, w_output)
 output_layer(hidden_layer(input_spikes))
@@ -41,7 +47,7 @@ output_layer.backward()
 t_plus = sum([x.time for x in output_layer.post_spikes])
 
 w_minus = w_hidden.copy()
-w_minus[0,0] -= w_eps
+w_minus[0, 0] -= w_eps
 hidden_layer = LIFLayer(hidden_parameters, w_minus)
 output_layer = LIFLayer(output_parameters, w_output)
 output_layer(hidden_layer(input_spikes))
@@ -49,7 +55,7 @@ for spike in output_layer.post_spikes:
     spike.error = 1
 output_layer.backward()
 t_minus = sum([x.time for x in output_layer.post_spikes])
-numerical_hidden_grad = (t_plus-t_minus)/(2*w_eps)
+numerical_hidden_grad = (t_plus - t_minus) / (2 * w_eps)
 
 # Calculate numerical gradient for output synapse
 w_plus = w_output + w_eps
@@ -69,7 +75,7 @@ for spike in output_layer.post_spikes:
     spike.error = 1
 output_layer.backward()
 t_minus = sum([x.time for x in output_layer.post_spikes])
-numerical_output_grad = (t_plus-t_minus)/(2*w_eps)
+numerical_output_grad = (t_plus - t_minus) / (2 * w_eps)
 
 
 # Get adjoint gradient and variables
@@ -84,15 +90,40 @@ for spike in output_layer.post_spikes:
 output_layer.backward()
 output_gradient = output_layer.gradient.copy()
 hidden_gradient = hidden_layer.gradient.copy()
-ts, output_voltage_trace = output_layer.get_voltage_trace_for_neuron(0, plot_t_max, dt=1e-4)
-ts, hidden_voltage_trace = hidden_layer.get_voltage_trace_for_neuron(0, plot_t_max, dt=1e-4)
+ts, output_voltage_trace = output_layer.get_voltage_trace_for_neuron(
+    0, plot_t_max, dt=1e-4
+)
+ts, hidden_voltage_trace = hidden_layer.get_voltage_trace_for_neuron(
+    0, plot_t_max, dt=1e-4
+)
 
-ts, output_lambda_trace = output_layer.get_lambda_i_trace_for_neuron(0, plot_t_max, dt=1e-4)
-output_lambdas = [output_layer.get_lambda_i_for_neuron(0, x.time) for x in hidden_spikes]
-ts, hidden_lambda_trace = hidden_layer.get_lambda_i_trace_for_neuron(0, plot_t_max, dt=1e-4)
-hidden_lambdas = [hidden_layer.get_lambda_i_for_neuron(0, x.time) for x in input_spikes if x.source_neuron == 0]
+ts, output_lambda_trace = output_layer.get_lambda_i_trace_for_neuron(
+    0, plot_t_max, dt=1e-4
+)
+output_lambdas = [
+    output_layer.get_lambda_i_for_neuron(0, x.time) for x in hidden_spikes
+]
+ts, hidden_lambda_trace = hidden_layer.get_lambda_i_trace_for_neuron(
+    0, plot_t_max, dt=1e-4
+)
+hidden_lambdas = [
+    hidden_layer.get_lambda_i_for_neuron(0, x.time)
+    for x in input_spikes
+    if x.source_neuron == 0
+]
 
-def plot(ts, pre_spike_times, voltage_trace, lambda_trace, lambdas, tau_syn, gradient, numerical_gradient, save_to):
+
+def plot(
+    ts,
+    pre_spike_times,
+    voltage_trace,
+    lambda_trace,
+    lambdas,
+    tau_syn,
+    gradient,
+    numerical_gradient,
+    save_to,
+):
     _, axs = plt.subplots(1, 3, sharex=True, sharey=False, figsize=(10, 3))
     axs = axs.flat
     plt.sca(axs[0])
@@ -119,9 +150,33 @@ def plot(ts, pre_spike_times, voltage_trace, lambda_trace, lambdas, tau_syn, gra
     plt.tight_layout()
     plt.savefig(save_to)
 
-print(f"Relative deviation of adjoint gradient from numerical: {abs(output_gradient[0,0]-numerical_output_grad)/numerical_output_grad} (output weight)")
-print(f"Relative deviation of adjoint gradient from numerical: {abs(hidden_gradient[0,0]-numerical_hidden_grad)/numerical_hidden_grad} (hidden weight)")
 
-plot(ts, [x.time for x in hidden_spikes], output_voltage_trace, output_lambda_trace, output_lambdas, output_layer.parameters.tau_syn, output_gradient, numerical_output_grad, "output_trace.svg")
-plot(ts, [x.time for x in input_spikes if x.source_neuron==0], hidden_voltage_trace, hidden_lambda_trace, hidden_lambdas, hidden_layer.parameters.tau_syn, hidden_gradient, numerical_hidden_grad, "hidden_trace.svg")
+print(
+    f"Relative deviation of adjoint gradient from numerical: {abs(output_gradient[0,0]-numerical_output_grad)/numerical_output_grad} (output weight)"
+)
+print(
+    f"Relative deviation of adjoint gradient from numerical: {abs(hidden_gradient[0,0]-numerical_hidden_grad)/numerical_hidden_grad} (hidden weight)"
+)
 
+plot(
+    ts,
+    [x.time for x in hidden_spikes],
+    output_voltage_trace,
+    output_lambda_trace,
+    output_lambdas,
+    output_layer.parameters.tau_syn,
+    output_gradient,
+    numerical_output_grad,
+    "output_trace.svg",
+)
+plot(
+    ts,
+    [x.time for x in input_spikes if x.source_neuron == 0],
+    hidden_voltage_trace,
+    hidden_lambda_trace,
+    hidden_lambdas,
+    hidden_layer.parameters.tau_syn,
+    hidden_gradient,
+    numerical_hidden_grad,
+    "hidden_trace.svg",
+)
--- paper/figure3.py
+++ paper/figure3.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         errors = 1 - np.array(accs)
         all_valid_errors.append(errors)
         all_valid_losses.append(losses)
-    plt.figure(figsize=(4,3))
+    plt.figure(figsize=(4, 3))
     for errors in all_valid_errors:
         plt.plot(errors, "k-", alpha=0.1)
     plt.plot(np.mean(all_valid_errors, axis=0))
@@ -34,9 +34,11 @@ if __name__ == "__main__":
     plt.xlabel("Epoch")
     plt.ylabel("Validation Error")
     plt.savefig("yinyang_errors.pdf")
-    print(f"Error statistics: {np.mean(np.array(all_valid_errors)[:, -1])} +- {np.std(np.array(all_valid_errors)[:, -1])}")
+    print(
+        f"Error statistics: {np.mean(np.array(all_valid_errors)[:, -1])} +- {np.std(np.array(all_valid_errors)[:, -1])}"
+    )
 
-    plt.figure(figsize=(4,3))
+    plt.figure(figsize=(4, 3))
     for losses in all_valid_losses:
         plt.plot(losses, "k-", alpha=0.1)
     plt.plot(np.mean(all_valid_losses, axis=0))
