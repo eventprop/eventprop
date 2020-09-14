7df48613e2f81c3dbb3427da58d6ae1f8ea14028
Timo C. Wunderlich <timo.wunderlich@charite.de>
2020-09-14 07:57:54 +0200

    Format and add data files

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 import setuptools
 
 cpp_ext = setuptools.Extension(
-    name = "eventprop.lif_layer_cpp",
-    sources = ["src/lif_layer_cpp.cpp"],
-    include_dirs = ["src/pybind11/include", "src/eigen"],
-    language = "c++",
-    extra_compile_args = ["-std=c++11", "-Ofast"]
+    name="eventprop.lif_layer_cpp",
+    sources=["src/lif_layer_cpp.cpp"],
+    include_dirs=["src/pybind11/include", "src/eigen"],
+    language="c++",
+    extra_compile_args=["-std=c++11", "-Ofast"],
 )
 
+yin_yang_dir = "eventprop/yin_yang_data_set/publication_data"
+
 setuptools.setup(
-    name = "eventprop",
-    version = "0.1",
-    author = "Timo C. Wunderlich",
-    packages = ["eventprop"],
-    python_requires = ">=3.8",
-    install_requires = ["numpy", "scipy", "dask"],
-    ext_modules = [cpp_ext],
+    name="eventprop",
+    version="0.1",
+    author="Timo C. Wunderlich",
+    packages=["eventprop"],
+    python_requires=">=3.8",
+    install_requires=["numpy", "scipy", "dask"],
+    ext_modules=[cpp_ext],
+    data_files=[
+        (yin_yang_dir, "test_labels.npy"),
+        (yin_yang_dir, "train_labels.npy"),
+        (yin_yang_dir, "valid_labels.npy"),
+        (yin_yang_dir, "test_samples.npy"),
+        (yin_yang_dir, "train_samples.npy"),
+        (yin_yang_dir, "valid_samples.npy"),
+    ],
 )
